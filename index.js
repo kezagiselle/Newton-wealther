3f96d3185f65b711fbc8f83993f145113091f1e4
kezagiselle <kezagiselle776@gmail.com>
2024-02-22 16:18:52 +0200

    weather

---
+++ index.js
@@ -0,0 +1,5 @@
+const kelvin = 293;
+const celsius = kelvin - 273;
+let newton = celsius * ( 33 / 100);
+newton = Math.floor(newton);
+console.log(`The temperature in newton is ${newton} degrees.`);
